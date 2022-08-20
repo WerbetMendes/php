a25fcce6ae474bb106bde080fa2b5a8a37c86ef2
WerbetMendes <werbetfm@gmail.com>
2022-08-20 11:54:58 -0300

    Update index.php

--- 4_variaveis/4_var_de_var/index.php
+++ 4_variaveis/4_var_de_var/index.php
@@ -7,6 +7,6 @@ echo "<br>";
 
 $$x = "variavel2"; // Repete-se o nome da variável com $. 
 
-echo $variavel1; // Na tela, é impresso o valor da variável2, mesmo com o nome da variável1.
+echo $variavel1; // Na tela, é impresso o valor de variavel2, mesmo com o nome de variavel1.
 
 ?>

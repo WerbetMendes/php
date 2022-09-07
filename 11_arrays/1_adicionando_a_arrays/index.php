f597a7bbe74a291e8d8df5e083d9677a4b9576c6
WerbetMendes <werbetfm@gmail.com>
2022-09-07 20:49:51 -0300

    create folder 11

---
+++ 11_arrays/1_adicionando_a_arrays/index.php
@@ -0,0 +1,38 @@
+<?php
+
+$arr = [];
+
+print_r($arr);
+echo "<br>";
+
+$arr[0] = 10;
+print_r($arr);
+echo "<br>";
+
+$arr[1] = 15;
+print_r($arr);
+echo "<br>";
+
+//Modificar valores
+$arr[1] += 15;
+print_r($arr);
+echo"<br>";
+
+
+//Array Associativo
+$arrayAssoc = [];
+print_r($arrayAssoc);
+echo"<br>";
+
+$arrayAssoc["Carro"] = "BMW";
+print_r($arrayAssoc);
+echo "<br>";
+
+$arrayAssoc["Avião"] = "Boeing";
+print_r($arrayAssoc);
+echo "<br>";
+
+$arrayAssoc["Carro"] = "Ferrari";
+print_r($arrayAssoc);
+
+

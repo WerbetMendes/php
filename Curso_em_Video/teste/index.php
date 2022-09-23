4ab0b837964b906ac4c5dab46a67599ea6f44e46
WerbetMendes <werbetfm@gmail.com>
2022-09-23 20:57:13 -0300

    Create folder Teste

---
+++ Curso_em_Video/teste/index.php
@@ -0,0 +1,19 @@
+<?php
+
+abstract class pai {
+    public function herdado() {
+     $this->sobrescrito();
+    }
+    protected function sobrescrito() {
+     echo 'pai';
+    }
+  }
+  class filha extends pai {
+    protected function sobrescrito() {
+     echo 'filha';
+    }
+  }
+  $teste = new filha();
+  $teste->herdado();
+
+  // Resultado: filha

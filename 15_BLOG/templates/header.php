3c9fea5da9e875c68c8e7f2818f9054909364b60
WerbetMendes <werbetfm@gmail.com>
2022-10-07 15:12:13 -0300

    update folder 15

--- 15_BLOG/templates/header.php
+++ 15_BLOG/templates/header.php
@@ -1,4 +1,5 @@
 
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -6,20 +7,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog Codar</title>
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
    <header>
         <a href="" id="logo">
-            <img src="/img/logo2.png" alt="Blog Codar">
+            <img src="img/logo.svg" alt="Blog Codar">
         </a>
-        <nav>
-            <ul id="navbar">
-                <li><a href="index.php">Home</a></li>
-                <li><a href="#">Categorias</a></li>
-                <li><a href="#">Sobre</a></li>
-                <li><a href="contato.php">contato</a></li>
-            </ul>
+        <nav id="menu">
+            <a href="index.php">Home</a>
+            <a href="#">Categorias</a>
+            <a href="#">Sobre</a>
+            <a href="contato.php">Contato</a>
         </nav>
    </header>
    

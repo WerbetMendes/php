10da546b953380322ff7a3b2c651b2cfeb51a0a9
WerbetMendes <werbetfm@gmail.com>
2022-10-07 12:03:30 -0300

    update

--- 15_BLOG/index.php
+++ 15_BLOG/index.php
@@ -1,3 +1,4 @@
+
 <?php
 include_once "templates/header.php";
 
@@ -7,3 +8,16 @@ include_once "templates/header.php";
 <?php
 include_once "templates/footer.php";
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+    
+</body>
+</html>
--- 15_BLOG/templates/header.php
+++ 15_BLOG/templates/header.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -5,14 +6,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog Codar</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/styles.css">
 </head>
 <body>
    <header>
-        <a href="#" id="logo">
-            <img src="../img/logo2.png" alt="logo Hora de Codar">
+        <a href="" id="logo">
+            <img src="/img/logo2.png" alt="Blog Codar">
         </a>
+        <nav>
+            <ul id="navbar">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Categorias</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="contato.php">contato</a></li>
+            </ul>
+        </nav>
    </header>
+   
 
+ 
+  
 </body>
 </html>

51cb1243f56eb2cd34da56600fc7c7fe12acee38
WerbetMendes <werbetfm@gmail.com>
2022-10-18 10:27:36 -0300

    update Agenda

--- 17_Agenda/templates/header.php
+++ 17_Agenda/templates/header.php
@@ -1,5 +1,12 @@
 <?php
-include_once("config/url.php")
+include_once("config/url.php");
+include_once("config/process.php");
+
+//limpa a mensagem
+if(isset($_SESSION['msg'])) {
+    $printMsg = $_SESSION['msg'];
+    $_SESSION['msg'] = '';
+}
 
 ?>
 
@@ -15,13 +22,13 @@ include_once("config/url.php")
     <!-- FONT AWESOME -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- CSS -->
-    <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a class="navbar-brand" href="<?= $BASE_URL ?>index.php">
-                <img src="<?= $BASE_URL ?>img/logo.svg" alt="Agenda">
+            <a class="navbar-brand" href="index.php">
+                <img src="img/logo.svg" alt="Agenda">
             </a>
             <div>
                 <div class="navbar-nav">

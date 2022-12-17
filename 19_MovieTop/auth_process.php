60dec21417821a389a55b13aec257822ac61bac4
WerbetMendes <werbetfm@gmail.com>
2022-12-16 21:56:18 -0300

    update

--- 19_MovieTop/auth_process.php
+++ 19_MovieTop/auth_process.php
@@ -8,7 +8,7 @@
 
     $message = new Message($BASE_URL);
 
-    $userDAO = new UserDAO($conn, $BASE_URL);
+    $userDao = new UserDAO($conn, $BASE_URL);
     
 //Verifica o tipo do formulário e eliminação de possível dado malicioso.
 $type = filter_input(INPUT_POST, "type");
@@ -66,5 +66,23 @@ if($type === "register") {
     }
 
 } else if($type === "login") {
+   
+    $email = filter_input(INPUT_POST, "email");
+    $password = filter_input(INPUT_POST, "password");
 
+    // Tenta autenticar usuário
+    if($userDao->authenticateUser($email, $password)) {
+
+        $message->setMessage("Seja bem-vindo!", "success", "editprofile.php");
+
+    // Redireciona o usuário, caso não consiga autenticar.
+    } else {
+
+        $message->setMessage("Usuário e/ou senha incorretos.", "error", "back");
+
+    }
+
+} else {
+
+    $message->setMessage("Informações inválidas!", "error", "index.php");
 }

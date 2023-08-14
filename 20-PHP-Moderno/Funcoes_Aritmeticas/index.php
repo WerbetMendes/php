4b61da497447634394f18824d74dd916e4afe3c9
WerbetMendes <werbetfm@gmail.com>
2023-08-14 12:18:58 -0300

    funcoes_aritmeticas

---
+++ 20-PHP-Moderno/Funcoes_Aritmeticas/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $r = abs(50);
+    print ("A resposta é $r<br>");
+    ?>
+
+    <?php
+    $r = min(5, 2);
+    print("A resposta é $r<br>");
+    ?>
+
+    <?php 
+    $r = max(10, 3, 6, 18, 20, 4, 8);
+    print("A resposta é $r<br>")
+    ?>
+
+    <?php
+   
+    $r = sqrt(81);
+    print("A resposta é $r");
+    ?>
+</body>
+</html>
